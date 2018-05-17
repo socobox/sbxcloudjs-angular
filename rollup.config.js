d5c3c41655053a31073cd0373b77721db3f1ec85
Luis Guzman <ing.luis.guzman.reyes@gmail.com>
2018-05-17 07:02:15 -0500

    update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,43 +1,29 @@
+const globals = {
+  '@angular/core': 'ng.core',
+  '@angular/common': 'ng.common',
+  '@angular/forms': 'ng.forms',
+  '@angular/common/http': 'ng.common.http',
+  'sbx-querybuilder/index': 'QueryBuilder',
+  'sbxcorejs': 'Find',
+  'ngx-cookie-service': 'ngxCookieService',
+  'rxjs/Observable': 'Rx',
+  'rxjs/add/operator/map': 'Rx.Observable.prototype',
+  'rxjs/add/operator/mergeAll': 'Rx.Observable.prototype',
+  'rxjs/add/operator/mergeMap': 'Rx.Observable.prototype',
+  'rxjs/add/operator/toPromise': 'Rx.Observable.prototype',
+  'rxjs/add/operator/toArray': 'Rx.Observable.prototype',
+  'rxjs/add/observable/of': 'Rx.Observable',
+  'rxjs/add/observable/forkJoin': 'Rx.Observable',
+  'rxjs/add/observable/merge': 'Rx.Observable'
+};
+
 export default {
-	entry: 'dist/index.js',
-	dest: 'dist/bundles/sbx.umd.js',
-	sourceMap: false,
-	format: 'umd',
-	moduleName: 'sbxangular',
-	globals: {
-		'@angular/core': 'ng.core',
-		'@angular/common': 'ng.common',
-		'@angular/forms': 'ng.forms',
-        '@angular/common/http': 'ng.common.http',
-        'sbx-querybuilder/index': 'QueryBuilder',
-        'sbxcorejs': 'Find',
-        'ngx-cookie-service': 'ngxCookieService',
-		'rxjs/Observable': 'Rx',
-		'rxjs/add/operator/map': 'Rx.Observable.prototype',
-		'rxjs/add/operator/mergeAll': 'Rx.Observable.prototype',
-		'rxjs/add/operator/mergeMap': 'Rx.Observable.prototype',
-		'rxjs/add/operator/toPromise': 'Rx.Observable.prototype',
-		'rxjs/add/operator/toArray': 'Rx.Observable.prototype',
-		'rxjs/add/observable/of': 'Rx.Observable',
-		'rxjs/add/observable/forkJoin': 'Rx.Observable',
-		'rxjs/add/observable/merge': 'Rx.Observable'
-	},
-    external: [
-        '@angular/core',
-        '@angular/common',
-        '@angular/common/http',
-        '@angular/forms',
-        'sbx-querybuilder/index',
-        'sbxcorejs',
-        'ngx-cookie-service',
-        'rxjs/Observable',
-        'rxjs/add/operator/map',
-        'rxjs/add/operator/mergeAll',
-        'rxjs/add/operator/mergeMap',
-        'rxjs/add/operator/toPromise',
-        'rxjs/add/operator/toArray',
-        'rxjs/add/observable/of',
-        'rxjs/add/observable/forkJoin',
-        'rxjs/add/observable/merge'
-	]
+  input: 'dist/index.js',
+  output: {
+    file: 'dist/bundles/sbx.umd.js',
+    format: 'umd',
+    globals: globals,
+    name: 'sbxangular'
+  },
+  external: Object.keys(globals)
 }
